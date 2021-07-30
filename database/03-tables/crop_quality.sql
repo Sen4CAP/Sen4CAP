2ca996749a10d3233c039c2262559b09f0962228
Laurențiu Nicola <lnicola@dend.ro>
2021-07-30 16:30:14 +0300

    Add crop_quality

---
+++ database/03-tables/crop_quality.sql
@@ -0,0 +1,4 @@
+create table crop_quality(
+    crop_quality_id smallint not null primary key,
+    description text not null
+);
---
+++ database/07-data/crop_quality.sql
@@ -0,0 +1,4 @@
+insert into crop_quality
+values
+    (1, 'Normal growing conditions'),
+    (2, 'Abnormal growing conditions (abandoned crop, young plantation that does not produce, climatic damage, erosion damage, disease, parasite damage)');
