213c36e551b7c48cdf2b8ad9da0a3c6eb5de30e7
Laurențiu Nicola <lnicola@dend.ro>
2021-07-30 16:23:11 +0300

    Add yield_estimation_method

---
+++ database/03-tables/yield_estimation_method.sql
@@ -0,0 +1,4 @@
+create table yield_estimation_method(
+    yield_estimation_method_id smallint not null,
+    description text not null
+);
---
+++ database/07-data/yield_estimation_method.sql
@@ -0,0 +1,5 @@
+insert into yield_estimation_method
+values
+    (1, 'Eye estimation'),
+    (2, 'Data provided by farmer'),
+    (3, 'Crop cutting');
