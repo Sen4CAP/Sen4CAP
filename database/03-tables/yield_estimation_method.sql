c24372b604fe5bf7e141d39c8f2a438745ee60a1
Laurențiu Nicola <lnicola@dend.ro>
2021-07-30 16:23:57 +0300

    Add constraint

--- database/03-tables/yield_estimation_method.sql
+++ database/03-tables/yield_estimation_method.sql
@@ -1,4 +1,4 @@
 create table yield_estimation_method(
-    yield_estimation_method_id smallint not null,
+    yield_estimation_method_id smallint not null primary key,
     description text not null
 );
