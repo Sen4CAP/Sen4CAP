aae77e562c02d903a0c2e531ac225198f5b1099e
Laurențiu Nicola <lnicola@dend.ro>
2021-08-13 19:32:38 +0300

    Add Other to the yield estimation methods

--- database/07-data/yield_estimation_method.sql
+++ database/07-data/yield_estimation_method.sql
@@ -2,4 +2,5 @@ insert into yield_estimation_method
 values
     (1, 'Eye estimation'),
     (2, 'Data provided by farmer'),
-    (3, 'Crop cutting');
+    (3, 'Crop cutting'),
+    (4, 'Other methods');
