b52f471e855a0ab5cba5c5b511a48c6417fd5404
Laurențiu Nicola <lnicola@dend.ro>
2021-08-26 20:31:45 +0300

    Add processor.insitu.path to config tables

---
+++ database/07-data/sen4stat/config.sql
@@ -0,0 +1,1 @@
+INSERT INTO config(key, site_id, value, last_updated) VALUES ('processor.insitu.path', NULL, '/mnt/archive/insitu/{site}/{year}', '2021-08-26 20:29:59.7325+03');
---
+++ database/07-data/sen4stat/config_metadata.sql
@@ -0,0 +1,1 @@
+INSERT INTO config_metadata VALUES ('processor.insitu.path', 'The path to the pre-processed in-situ data products', 'string', false, 21, FALSE, 'The path to the pre-processed in-situ data products', NULL);
