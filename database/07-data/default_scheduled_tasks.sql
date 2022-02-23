424f1038d9c1a6342a76778b7123e0acc4826852
Cosmin Udroiu <cosmin.udroiu@c-s.ro>
2022-02-23 15:43:33 +0200

    Split of composite into s1 and bi composite in default_scheduled_tasks.sql.

--- database/07-data/default_scheduled_tasks.sql
+++ database/07-data/default_scheduled_tasks.sql
@@ -7,4 +7,5 @@ values (2, 'L3A', 2, 0, 31, 'start', 'month', '1 month - 1 day', 60, 1, null),
        (10, 'S4C_L4B', 2, 0, 31, 'start', null, '31 days', 60, 1, null),
        (11, 'S4C_L4C', 1, 7, 0, 'start', null, '7 days', 60, 1, null),
        (14, 'S4C_MDB1', 1, 1, 0, 'start', null, '1 day', 60, 1, null),
-       (22, 'L3COMP', 2, 0, 31, 'start', 'month', '1 month - 1 day', 60, 1, null);
+       (22, 'L3_S1_COMP', 2, 0, 31, 'start', 'month', '1 month - 1 day', 60, 1, null),
+       (23, 'L3_IND_COMP', 2, 0, 31, 'start', 'month', '1 month - 1 day', 60, 1, null);
