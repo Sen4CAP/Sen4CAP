ff3a9bdd51d79f1f030102dd6a46c2242537fbdc
Laurențiu Nicola <lnicola@dend.ro>
2022-12-30 20:57:50 +0200

    Remove 0 values from the default crop remapping table

--- database/07-data/sen4stat/crop_remapping_set_detail.sql
+++ database/07-data/sen4stat/crop_remapping_set_detail.sql
@@ -1,5 +1,4 @@
 COPY public.crop_remapping_set_detail (crop_remapping_set_id, original_code, remapped_code_pre, description_pre, remapped_code_post, description_post) FROM stdin;
-1	0	0	Remove	0	Remove
 1	1111	1111	Winter wheat	111	Wheat
 1	1112	1112	Spring wheat	111	Wheat
 1	1113	1113	Hard wheat	111	Wheat
@@ -13,7 +12,6 @@ COPY public.crop_remapping_set_detail (crop_remapping_set_id, original_code, rem
 1	1161	1161	Rye	116	Rye
 1	1171	1171	Oats	117	Oats
 1	1181	1181	Millets	118	Millets
-1	1191	0	Remove	0	Remove
 1	1192	1192	Other cereals	119	Other cereals
 1	1193	1193	Quinoa	119	Other cereals
 1	1211	121	Leafy or stem vegetables	121	Leafy or stem vegetables
@@ -41,7 +39,6 @@ COPY public.crop_remapping_set_detail (crop_remapping_set_id, original_code, rem
 1	1236	1236	Beetroots	123	Root, bulb or tuberous vegetables
 1	1239	1239	Other root, buld or tuberous vegetables	123	Root, bulb or tuberous vegetables
 1	1241	1241	Mushrooms	124	Mushrooms and truffles
-1	1291	0	Remove	0	Remove
 1	1411	1411	Soya beans	141	Soya beans
 1	1421	1421	Groundnuts	142	Groundnuts
 1	1431	1431	Castor bean	143	Other oilseed crops
@@ -52,12 +49,10 @@ COPY public.crop_remapping_set_detail (crop_remapping_set_id, original_code, rem
 1	1436	1436	Safflower	143	Other oilseed crops
 1	1437	1437	Sesame	143	Other oilseed crops
 1	1438	1438	Sunflower	143	Other oilseed crops
-1	1439	0	Remove	0	Remove
 1	1511	1511	Potatoes	151	Potatoes
 1	1521	1521	Sweet potatoes	152	Sweet potatoes
 1	1531	1531	Cassava	153	Cassava
 1	1541	1541	Yams	154	Yams
-1	1591	0	Remove	0	Remove
 1	1611	161	Spice crops	161	Spice crops
 1	1612	161	Spice crops	161	Spice crops
 1	1613	161	Spice crops	161	Spice crops
@@ -70,23 +65,18 @@ COPY public.crop_remapping_set_detail (crop_remapping_set_id, original_code, rem
 1	1761	1761	Lupins	17	Leguminous crops
 1	1771	1771	Peas	17	Leguminous crops
 1	1781	1781	Pigeon peas	17	Leguminous crops
-1	1791	0	Remove	0	Remove
 1	1811	1811	Sugar beet	181	Sugar beet
 1	1821	1821	Sugar cane	182	Sugar cane
 1	1831	1141	Sorghum	114	Sorghum
-1	1891	0	Remove	0	Remove
 1	1911	1911	Alfalfa	17	Leguminous crops
 1	1912	1912	Vetches	17	Leguminous crops
 1	1919	3	Grassland and meadows	3	Grassland and meadows
 1	1921	1921	Cotton	192	Fibre crops
 1	1922	1922	Jute, kenaf and other similar crops	192	Fibre crops
 1	1923	1923	Flax, hemp and other similar crops	192	Fibre crops
-1	1929	0	Remove	0	Remove
 1	1931	1931	Medicinal, aromatic, pesticidal or similar crops	1931	Medicinal, aromatic, pesticidal or similar crops
 1	1941	1941	Flowers crops	1941	Flowers crops
 1	1991	1991	Tobacco	1991	Tobacco
-1	1992	0	Remove	0	Remove
-1	1999	0	Remove	0	Remove
 1	2111	21	Fruits trees	21	Fruits trees
 1	2112	21	Fruits trees	21	Fruits trees
 1	2121	21	Fruits trees	21	Fruits trees
@@ -113,16 +103,10 @@ COPY public.crop_remapping_set_detail (crop_remapping_set_id, original_code, rem
 1	2419	24	Trees	24	Trees
 1	2911	291	Succulent plant	291	Succulent plant
 1	2912	291	Succulent plant	291	Succulent plant
-1	2999	0	Remove	0	Remove
 1	3111	3	Grassland and meadows	3	Grassland and meadows
 1	3112	3	Grassland and meadows	3	Grassland and meadows
 1	3113	3	Grassland and meadows	3	Grassland and meadows
 1	3199	3	Grassland and meadows	3	Grassland and meadows
-1	4111	0	Remove	0	Remove
-1	4112	0	Remove	0	Remove
-1	4113	0	Remove	0	Remove
-1	4114	0	Remove	0	Remove
-1	4115	0	Remove	0	Remove
 1	5111	5	Shrub land	5	Shrub land
 1	6111	61	Conifers	6	Forest
 1	6211	62	Deciduous	6	Forest
