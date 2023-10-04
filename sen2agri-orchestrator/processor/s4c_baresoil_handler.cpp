48073526b47559c18b287c37ef79541c820b4012
Cosmin Udroiu <cosmin.udroiu@c-s.ro>
2023-10-04 16:19:32 +0300

    Added bare soil orchestrator handler

---
+++ sen2agri-orchestrator/processor/s4c_baresoil_handler.cpp
@@ -0,0 +1,317 @@
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QRegularExpression>
+#include <fstream>
+
+#include "logger.hpp"
+#include "processorhandlerhelper.h"
+#include "s4c_baresoil_handler.hpp"
+#include "stepexecutiondecorator.h"
+
+#include "products/generichighlevelproducthelper.h"
+using namespace orchestrator::products;
+
+QList<std::reference_wrapper<TaskToSubmit>>
+S4CBareSoilHandler::CreateTasks(const S4CBareSoilJobConfig &, QList<TaskToSubmit> &outAllTasksList)
+{
+    int curTaskIdx = 0;
+    outAllTasksList.append(TaskToSubmit{ "s4c-bare-soil-s2-calibration", {} });
+    int s2CalibIdx = curTaskIdx++;
+    outAllTasksList.append(TaskToSubmit{ "s4c-bare-soil-s1-calibration", {outAllTasksList[s2CalibIdx]}  });
+    int s1CalibIdx = curTaskIdx++;
+    outAllTasksList.append(TaskToSubmit{ "s4c-bare-soil-s2-model", {outAllTasksList[s2CalibIdx]}  });
+    int s2ModelIdx = curTaskIdx++;
+    outAllTasksList.append(TaskToSubmit{ "s4c-bare-soil-s1-model", {outAllTasksList[s1CalibIdx]}  });
+    int s1ModelIdx = curTaskIdx++;
+
+    outAllTasksList.append(TaskToSubmit{ "s4c-bare-soil-markers", {outAllTasksList[s2ModelIdx], outAllTasksList[s1ModelIdx]}  });
+    int markersIdx = curTaskIdx++;
+
+    outAllTasksList.append(TaskToSubmit{ "product-formatter", {outAllTasksList[markersIdx]} });
+
+    QList<std::reference_wrapper<TaskToSubmit>> allTasksListRef;
+    for (TaskToSubmit &task : outAllTasksList) {
+        allTasksListRef.append(task);
+    }
+    return allTasksListRef;
+}
+
+NewStepList S4CBareSoilHandler::CreateSteps(QList<TaskToSubmit> &allTasksList,const S4CBareSoilJobConfig &cfg)
+{
+    int curTaskIdx = 0;
+    NewStepList allSteps;
+    QStringList prdFormatterFiles;
+
+    TaskToSubmit &s2CalibTask = allTasksList[curTaskIdx++];
+    TaskToSubmit &s1CalibTask = allTasksList[curTaskIdx++];
+    TaskToSubmit &s2ModelTask = allTasksList[curTaskIdx++];
+    TaskToSubmit &s1ModelTask = allTasksList[curTaskIdx++];
+    TaskToSubmit &markersTask = allTasksList[curTaskIdx++];
+    TaskToSubmit &productFormatterTask = allTasksList[curTaskIdx++];
+
+    // Resulting files from tasks
+    const QString &s2CalibPath = s2CalibTask.GetFilePath("L4E_BS_CalibrationS2.csv");
+    const QString &s1CalibPath = s1CalibTask.GetFilePath("L4E_BS_CalibrationS1.csv");
+
+    const QString &s2ResultsPath = s2ModelTask.GetFilePath("L4E_BS_S2results.csv");
+    const QString &s2ModelPath = s2ModelTask.GetFilePath("L4E_BSmodelS2.sav");
+    const QString &s2FigImportancePath = s2ModelTask.GetFilePath("BSmodelS2featuresimportance.png");
+
+    const QString &s1ResultsPath = s1ModelTask.GetFilePath("L4E_BS_S1results.csv");
+    const QString &s1ModelPath = s1ModelTask.GetFilePath("L4E_BSmodelS1.sav");
+    const QString &s1FigImportancePath = s1ModelTask.GetFilePath("BSmodelS2featuresimportance.png");
+
+    const QString &s2MarkersPath = markersTask.GetFilePath("L4E_BS_MarkersS2.csv");
+    const QString &s1MarkersPath = markersTask.GetFilePath("L4E_BS_MarkersS1.csv");
+    const QString &allMarkersPath = markersTask.GetFilePath("L4E_BS_MarkersAll.csv");
+
+    // Inputs extraction and reflectances stack tif creation
+    const QStringList &s2CalibrationArgs = GetS2CalibrationTaskArgs(cfg, s2CalibPath);
+    allSteps.append(CreateTaskStep(s2CalibTask, "S2Calibration", s2CalibrationArgs ));
+
+    const QStringList &s1CalibrationArgs = GetS1CalibrationTaskArgs(cfg, s2CalibPath, s1CalibPath);
+    allSteps.append(CreateTaskStep(s1CalibTask, "S1Calibration", s1CalibrationArgs));
+
+    const QStringList &s2ModelArgs = GetS2ModelTaskArgs(cfg, s2CalibPath, s2ResultsPath, s2ModelPath, s2FigImportancePath);
+    allSteps.append(CreateTaskStep(s2ModelTask, "S2Model", s2ModelArgs));
+
+    const QStringList &s1ModelArgs = GetS1ModelTaskArgs(cfg, s1CalibPath, s1ResultsPath, s1ModelPath, s1FigImportancePath);
+    allSteps.append(CreateTaskStep(s1ModelTask, "S1Model", s1ModelArgs));
+
+    const QStringList &markersArgs = GetMarkersTaskArgs(cfg, s2ResultsPath, s1ResultsPath, s2MarkersPath, s1MarkersPath, allMarkersPath);
+    allSteps.append(CreateTaskStep(markersTask, "MarkersExtraction", markersArgs));
+
+    prdFormatterFiles.append(s2CalibPath);
+    prdFormatterFiles.append(s1CalibPath);
+    prdFormatterFiles.append(s2ResultsPath);
+    prdFormatterFiles.append(s2ModelPath);
+    prdFormatterFiles.append(s2FigImportancePath);
+    prdFormatterFiles.append(s1ResultsPath);
+    prdFormatterFiles.append(s1ModelPath);
+    prdFormatterFiles.append(s1FigImportancePath);
+
+    const QStringList &productFormatterArgs = GetProductFormatterArgs(productFormatterTask, cfg, prdFormatterFiles);
+    allSteps.append(CreateTaskStep(productFormatterTask, "ProductFormatter", productFormatterArgs));
+
+    return allSteps;
+}
+
+QStringList S4CBareSoilHandler::GetS2CalibrationTaskArgs(const S4CBareSoilJobConfig &cfg, const QString &s2CalibPath)
+{
+    QStringList args = {
+                "--input", cfg.mdb1PrdPath,
+                "--output", s2CalibPath,
+                "--lpis", cfg.lpisPath,
+                "--thr-bs-ndvi", QString::number(cfg.calibBSNdviThr),
+                "--thr-nbs-ndvi", QString::number(cfg.calibNBSNdviThr),
+                "--thr-bs-ndwi", QString::number(cfg.calibBSNdwiThr),
+                "--thr-nbs-ndwi", QString::number(cfg.calibNBSNdwiThr),
+                "--thr-bs-ndti", QString::number(cfg.calibBSNdtiThr),
+                "--thr-nbs-ndti", QString::number(cfg.calibNBSNdtiThr),
+                "--thr-nbs-fcover", QString::number(cfg.calibNBSFcoverThr),
+                "--tiles"
+    };
+    args += cfg.siteTiles;
+
+    return args;
+}
+
+QStringList S4CBareSoilHandler::GetS1CalibrationTaskArgs(const S4CBareSoilJobConfig &cfg, const QString &s2CalibPath, const QString &s1CalibPath)
+{
+    return {    "--input", cfg.mdb1PrdPath,
+                "--output", s1CalibPath,
+                "--s2-bs-calib", s2CalibPath
+    };
+}
+
+QStringList S4CBareSoilHandler::GetS2ModelTaskArgs(const S4CBareSoilJobConfig &cfg, const QString &s2CalibPath,
+                                                   const QString &s2Results, const QString &outputModel,
+                                                   const QString &figImportancePath)
+{
+    QStringList args =  {
+                "--input", cfg.mdb1PrdPath,
+                "--output", s2Results,
+                "--s2-bs-calib", s2CalibPath,
+                "--estimators-number", QString::number(cfg.modelEstimatorsNo),
+                "--output-model", outputModel,
+                "--output-fig-importance", figImportancePath,
+                "--tiles"
+    };
+    args += cfg.siteTiles;
+
+    return args;
+}
+
+QStringList S4CBareSoilHandler::GetS1ModelTaskArgs(const S4CBareSoilJobConfig &cfg, const QString &s1CalibPath,
+                                                   const QString &s1Results, const QString &outputModel,
+                                                   const QString &figImportancePath)
+{
+    return {    "--input", cfg.mdb1PrdPath,
+                "--output", s1Results,
+                "--s1-bs-calib", s1CalibPath,
+                "--estimators-number", QString::number(cfg.modelEstimatorsNo),
+                "--output-model", outputModel,
+                "--output-fig-importance", figImportancePath
+    };
+}
+
+QStringList S4CBareSoilHandler::GetMarkersTaskArgs(const S4CBareSoilJobConfig &cfg, const QString &s2Results,
+                                                   const QString &s1Results, const QString &outputS2Markers,
+                                                   const QString &outputS1Markers, const QString &outputAllMarkers)
+{
+    return {    "--input-s2", s2Results,
+                "--input-s1", s1Results,
+                "--year", QString::number(cfg.year),
+                "--out-markers-s2", outputS2Markers,
+                "--out-markers-s1", outputS1Markers,
+                "--out-markers-all", outputAllMarkers,
+                "--start-date", cfg.startDate.toString("yyyy-MM-dd"),
+                "--end-date", cfg.endDate.toString("yyyy-MM-dd"),
+                "--p-long", QString::number(cfg.markersLongPeriod),
+                "--p-short", QString::number(cfg.markersShortPeriod),
+                "--s2-periods", QString::number(cfg.markersS2PeriodsNo),
+                "--s1-periods", QString::number(cfg.markersS1PeriodsNo),
+                "--thr-bs-s2", QString::number(cfg.markersS2BSThreshold),
+                "--thr-nbs-s2", QString::number(cfg.markersS2NBSThreshold),
+                "--thr-bs-s1",  QString::number(cfg.markersS1BSThreshold),
+                "--thr-nbs-s1",  QString::number(cfg.markersS1NBSThreshold)
+    };
+}
+
+
+
+void S4CBareSoilHandler::HandleJobSubmittedImpl(EventProcessingContext &ctx,
+                                                const JobSubmittedEvent &event)
+{
+    S4CBareSoilJobConfig cfg(&ctx, event);
+    QList<TaskToSubmit> allTasksList;
+    QList<std::reference_wrapper<TaskToSubmit>> allTasksListRef = CreateTasks(cfg, allTasksList);
+    SubmitTasks(ctx, cfg.event.jobId, allTasksListRef);
+    NewStepList allSteps = CreateSteps(allTasksList, cfg);
+    ctx.SubmitSteps(allSteps);
+}
+
+void S4CBareSoilHandler::HandleTaskFinishedImpl(EventProcessingContext &ctx,
+                                                const TaskFinishedEvent &event)
+{
+    if (event.module == "product-formatter") {
+        const QString &prodName = GetOutputProductName(ctx, event);
+        const QString &productFolder =
+            GetFinalProductFolder(ctx, event.jobId, event.siteId) + "/" + prodName;
+        if (prodName != "") {
+            const QString &quicklook = GetProductFormatterQuicklook(ctx, event);
+            const QString &footPrint = GetProductFormatterFootprint(ctx, event);
+            // Insert the product into the database
+            GenericHighLevelProductHelper prdHelper(productFolder);
+            int prdId = ctx.InsertProduct({ ProductType::S4SYieldFeatProductTypeId, event.processorId,
+                                            event.siteId, event.jobId, productFolder, prdHelper.GetAcqDate(),
+                                            prodName, quicklook, footPrint,
+                                            std::experimental::nullopt, TileIdList(), ProductIdsList() });
+            const QString &prodFolderOutPath =
+                ctx.GetOutputPath(event.jobId, event.taskId, event.module,
+                                  processorDescr.shortName) +
+                "/" + "prd_infos.txt";
+
+            QFile file(prodFolderOutPath);
+            if (file.open(QIODevice::ReadWrite)) {
+                QTextStream stream(&file);
+                stream << prdId << ';' << productFolder << '\n';
+            }
+            ctx.MarkJobFinished(event.jobId);
+            // Now remove the job folder containing temporary files
+            // TODO: check why it still remove the folder even if the key is set to 1
+            RemoveJobFolder(ctx, event.jobId, processorDescr.shortName);
+        } else {
+            ctx.MarkJobFailed(event.jobId);
+            Logger::error(
+                QStringLiteral("Cannot insert into database the product with name %1 and folder %2")
+                    .arg(prodName)
+                    .arg(productFolder));
+        }
+    }
+}
+
+ProcessorJobDefinitionParams S4CBareSoilHandler::GetProcessingDefinitionImpl(
+    SchedulingContext &ctx,
+    int siteId,
+    int scheduledDate,
+    const ConfigurationParameterValueMap &requestOverrideCfgValues)
+{
+    ProcessorJobDefinitionParams params;
+
+    QDateTime seasonStartDate;
+    QDateTime seasonEndDate;
+    // extract the scheduled date
+    QDateTime qScheduledDate = QDateTime::fromTime_t(scheduledDate);
+    bool success = GetSeasonStartEndDates(ctx, siteId, seasonStartDate, seasonEndDate,
+                                          qScheduledDate, requestOverrideCfgValues);
+    // if cannot get the season dates
+    if (!success) {
+        Logger::debug(QStringLiteral("Scheduler Bare Soil: Error getting season start dates for "
+                                     "site %1 for scheduled date %2!")
+                          .arg(siteId)
+                          .arg(qScheduledDate.toString()));
+        return params;
+    }
+
+    QDateTime limitDate = seasonEndDate.addMonths(2);
+    if (qScheduledDate > limitDate) {
+        Logger::debug(QStringLiteral("Scheduler Bare Soil: Error scheduled date %1 greater than the "
+                                     "limit date %2 for site %3!")
+                          .arg(qScheduledDate.toString())
+                          .arg(limitDate.toString())
+                          .arg(siteId));
+        return params;
+    }
+
+    ConfigurationParameterValueMap cfgValues =
+        ctx.GetConfigurationParameters(S4C_BARE_SOIL_CFG_PREFIX, siteId, requestOverrideCfgValues);
+    // we might have an offset in days from starting the downloading products to start the S4C L4A
+    // production
+    int startSeasonOffset = cfgValues[QStringLiteral(S4C_BARE_SOIL_CFG_PREFIX) + "start_season_offset"].value.toInt();
+    seasonStartDate = seasonStartDate.addDays(startSeasonOffset);
+
+    QDateTime startDate = seasonStartDate;
+    QDateTime endDate = qScheduledDate;
+    // do not pass anymore the product list but the dates
+    params.jsonParameters.append("{ \"scheduled_job\": \"1\", \"start_date\": \"" + startDate.toString("yyyyMMdd") + "\", " +
+                                 "\"end_date\": \"" + endDate.toString("yyyyMMdd") + "\", " +
+                                 "\"season_start_date\": \"" + seasonStartDate.toString("yyyyMMdd") + "\", " +
+                                 "\"season_end_date\": \"" + seasonEndDate.toString("yyyyMMdd") + "\"}");
+
+    // Normally, we need at least 1 product available, the crop mask and the shapefile in order to
+    // be able to create a S4C Bare soil product but if we do not return here, the schedule block waiting
+    // for products (that might never happen)
+    bool waitForAvailProcInputs =
+        (cfgValues[QStringLiteral(S4C_BARE_SOIL_CFG_PREFIX) + "sched_wait_proc_inputs"].value.toInt() != 0);
+    if ((waitForAvailProcInputs == false) || ((params.productList.size() > 0))) {
+        params.isValid = true;
+        Logger::debug(
+            QStringLiteral("Executing scheduled job. Scheduler extracted for S4C L4A a number "
+                           "of %1 products for site ID %2 with start date %3 and end date %4!")
+                .arg(params.productList.size())
+                .arg(siteId)
+                .arg(startDate.toString())
+                .arg(endDate.toString()));
+    } else {
+        Logger::debug(QStringLiteral("Scheduled job for S4C Bare Soil and site ID %1 with start date %2 "
+                                     "and end date %3 will not be executed "
+                                     "(productsNo = %4)!")
+                          .arg(siteId)
+                          .arg(startDate.toString())
+                          .arg(endDate.toString())
+                          .arg(params.productList.size()));
+    }
+
+    return params;
+}
+
+QStringList S4CBareSoilHandler::GetProductFormatterArgs(TaskToSubmit &productFormatterTask,
+                                                     const S4CBareSoilJobConfig &cfg, const QStringList &listFiles) {
+    QString strTimePeriod = cfg.startDate.toString("yyyyMMddTHHmmss").append("_").append(cfg.endDate.toString("yyyyMMddTHHmmss"));
+    QStringList additionalArgs = {"-processor.generic.files"};
+    additionalArgs += listFiles;
+    return GetDefaultProductFormatterArgs(*(cfg.pCtx), productFormatterTask, cfg.event.jobId, cfg.event.siteId, "S4S_YIELDFEAT", strTimePeriod,
+                                         "generic", additionalArgs, true);
+}
+
---
+++ sen2agri-orchestrator/processor/s4c_baresoil_handler.hpp
@@ -0,0 +1,168 @@
+#pragma once
+
+#include "processorhandler.hpp"
+#include "optional.hpp"
+#include "s4c_mdb1_dataextract_steps_builder.hpp"
+#include "products/generichighlevelproducthelper.h"
+
+#define S4C_BARE_SOIL_CFG_PREFIX "processor.s4c_bare_soil."
+
+class S4CBareSoilHandler : public ProcessorHandler
+{
+    typedef struct S4CBareSoilJobConfig {
+        S4CBareSoilJobConfig(EventProcessingContext *pContext, const JobSubmittedEvent &evt)
+            : event(evt), isScheduled(false) {
+            pCtx = pContext;
+            siteShortName = pContext->GetSiteShortName(evt.siteId);
+            configParameters = pCtx->GetJobConfigurationParameters(evt.jobId, S4C_BARE_SOIL_CFG_PREFIX);
+            parameters = QJsonDocument::fromJson(evt.parametersJson.toUtf8()).object();
+
+            startDate = ProcessorHandlerHelper::GetDateTimeFromString(
+                        ProcessorHandlerHelper::GetStringConfigValue(parameters, configParameters, "start_date", S4C_BARE_SOIL_CFG_PREFIX));
+            endDate = ProcessorHandlerHelper::GetDateTimeFromString(
+                        ProcessorHandlerHelper::GetStringConfigValue(parameters, configParameters, "end_date", S4C_BARE_SOIL_CFG_PREFIX));
+
+            year = endDate.date().year();           // TODO: see if this is valid
+
+            const TileList &tiles = pCtx->GetSiteTiles(event.siteId, (int)Satellite::Sentinel2);
+            if (tiles.size() == 0) {
+                pCtx->MarkJobFailed(event.jobId);
+                throw std::runtime_error(
+                    QStringLiteral("Bare Soil: No tiles defined for site with id = %1").arg(evt.siteId).toStdString());
+            }
+            std::transform(tiles.cbegin(), tiles.cend(), std::back_inserter(siteTiles), [](const Tile & tile) {return tile.tileId ; } );
+
+            lpisPath = ExtractLpisPath(year);
+
+            const ProductList &mdb1PrdsList = pCtx->GetProducts(event.siteId, (int)ProductType::S4MDB1ProductTypeId,
+                                                                               startDate, endDate);
+            if (mdb1PrdsList.size() == 0) {
+                pCtx->MarkJobFailed(event.jobId);
+                throw std::runtime_error(QStringLiteral("Bare Soil: No MDB1 products were found in database for site %1 and interval %2 - %3.")
+                                         .arg(siteShortName)
+                                         .arg(startDate.toString())
+                                         .arg(endDate.toString()).toStdString());
+            }
+            const ProductList &mdbL4APrdsList = pCtx->GetProducts(event.siteId, (int)ProductType::S4MDBL4ASarMainProductTypeId,
+                                                                               startDate, endDate);
+            if (mdbL4APrdsList.size() == 0) {
+                pCtx->MarkJobFailed(event.jobId);
+                throw std::runtime_error(QStringLiteral("Bare Soil: No MDB L4A Sar Main products were found in database for site %1 and interval %2 - %3.")
+                                         .arg(siteShortName)
+                                         .arg(startDate.toString())
+                                         .arg(endDate.toString()).toStdString());
+            }
+            mdb1PrdPath = mdb1PrdsList.at(mdb1PrdsList.size()-1).fullPath;
+            mdbL4SarMainPrdPath = mdbL4APrdsList.at(mdbL4APrdsList.size()-1).fullPath;
+
+            calibBSNdviThr = ProcessorHandlerHelper::GetFloatConfigValue(parameters, configParameters, "calib_bs_ndvi_thr", S4C_BARE_SOIL_CFG_PREFIX, 0.15);
+            calibNBSNdviThr = ProcessorHandlerHelper::GetFloatConfigValue(parameters, configParameters, "calib_nbs_ndvi_thr", S4C_BARE_SOIL_CFG_PREFIX, 0.45);
+
+            calibBSNdwiThr = ProcessorHandlerHelper::GetFloatConfigValue(parameters, configParameters, "calib_bs_ndwi_thr", S4C_BARE_SOIL_CFG_PREFIX, 0.);
+            calibNBSNdwiThr = ProcessorHandlerHelper::GetFloatConfigValue(parameters, configParameters, "calib_nbs_ndwi_thr", S4C_BARE_SOIL_CFG_PREFIX, 0.3);
+
+            calibBSNdtiThr = ProcessorHandlerHelper::GetFloatConfigValue(parameters, configParameters, "calib_bs_ndti_thr", S4C_BARE_SOIL_CFG_PREFIX, 0.1);
+            calibNBSNdtiThr = ProcessorHandlerHelper::GetFloatConfigValue(parameters, configParameters, "calib_nbs_ndti_thr", S4C_BARE_SOIL_CFG_PREFIX, 0.25);
+
+            calibNBSFcoverThr = ProcessorHandlerHelper::GetFloatConfigValue(parameters, configParameters, "calib_nbs_fcover_thr", S4C_BARE_SOIL_CFG_PREFIX, 0.01);
+
+            modelEstimatorsNo = ProcessorHandlerHelper::GetIntConfigValue(parameters, configParameters, "model_estimator_no", S4C_BARE_SOIL_CFG_PREFIX, 30);
+
+            markersLongPeriod = ProcessorHandlerHelper::GetIntConfigValue(parameters, configParameters, "markers_long_period", S4C_BARE_SOIL_CFG_PREFIX, 60);
+            markersShortPeriod = ProcessorHandlerHelper::GetIntConfigValue(parameters, configParameters, "markers_short_period", S4C_BARE_SOIL_CFG_PREFIX, 30);
+            markersS2PeriodsNo = ProcessorHandlerHelper::GetIntConfigValue(parameters, configParameters, "markers_s2_periods_no", S4C_BARE_SOIL_CFG_PREFIX, 3);
+            markersS1PeriodsNo = ProcessorHandlerHelper::GetIntConfigValue(parameters, configParameters, "markers_s1_periods_no", S4C_BARE_SOIL_CFG_PREFIX, 4);
+
+            markersS2BSThreshold = ProcessorHandlerHelper::GetFloatConfigValue(parameters, configParameters, "markers_bs_s2_threshold", S4C_BARE_SOIL_CFG_PREFIX, 0.75);
+            markersS2NBSThreshold = ProcessorHandlerHelper::GetFloatConfigValue(parameters, configParameters, "markers_nbs_s2_threshold", S4C_BARE_SOIL_CFG_PREFIX, 0.8);
+            markersS1BSThreshold = ProcessorHandlerHelper::GetFloatConfigValue(parameters, configParameters, "markers_bs_s1_threshold", S4C_BARE_SOIL_CFG_PREFIX, 0.65);
+            markersS1NBSThreshold = ProcessorHandlerHelper::GetFloatConfigValue(parameters, configParameters, "markers_nbs_s1_threshold", S4C_BARE_SOIL_CFG_PREFIX, 0.7);
+        }
+
+        QString ExtractLpisPath(int year) {
+            // We take it the last LPIS product for this site.
+            const ProductList &lpisPrds = S4CUtils::GetLpisProduct(pCtx, event.siteId);
+            if (lpisPrds.size() == 0) {
+                pCtx->MarkJobFailed(event.jobId);
+                throw std::runtime_error(QStringLiteral("No LPIS product found in database for the MDB1 execution for site %1.").
+                                         arg(siteShortName).toStdString());
+            }
+
+            Product retLpisPrd;
+            for(const Product &lpisPrd: lpisPrds) {
+                if (lpisPrd.created.date().year() == year) {
+                    if (!retLpisPrd.created.isValid() || (lpisPrd.inserted > retLpisPrd.inserted)) {
+                        retLpisPrd = lpisPrd;
+                    }
+                }
+            }
+            if (retLpisPrd.fullPath.size() == 0) {
+                pCtx->MarkJobFailed(event.jobId);
+                throw std::runtime_error(QStringLiteral("Bare Soil: No LPIS product was found in database for site %1 and year %2.")
+                                         .arg(siteShortName)
+                                         .arg(year).toStdString());
+            }
+
+            return retLpisPrd.fullPath;
+        }
+
+        EventProcessingContext *pCtx;
+        JobSubmittedEvent event;
+
+        QString siteShortName;
+        QDateTime startDate;
+        QDateTime endDate;
+        QStringList siteTiles;
+        QString mdb1PrdPath;
+        QString mdbL4SarMainPrdPath;
+        QString lpisPath;
+
+        float calibBSNdviThr;
+        float calibNBSNdviThr;
+
+        float calibBSNdwiThr;
+        float calibNBSNdwiThr;
+
+        float calibBSNdtiThr;
+        float calibNBSNdtiThr;
+
+        float calibNBSFcoverThr;
+
+        int modelEstimatorsNo;
+
+        int markersLongPeriod;
+        int markersShortPeriod;
+        int markersS2PeriodsNo;
+        int markersS1PeriodsNo;
+        float markersS2BSThreshold;
+        float markersS2NBSThreshold;
+        float markersS1BSThreshold;
+        float markersS1NBSThreshold;
+
+        std::map<QString, QString> configParameters;
+        QJsonObject parameters;
+        bool isScheduled;
+        int year;
+
+    } S4CBareSoilJobConfig;
+
+private:
+    void HandleJobSubmittedImpl(EventProcessingContext &ctx,
+                                const JobSubmittedEvent &event) override;
+    void HandleTaskFinishedImpl(EventProcessingContext &ctx,
+                                const TaskFinishedEvent &event) override;
+
+    ProcessorJobDefinitionParams GetProcessingDefinitionImpl(SchedulingContext &ctx, int siteId, int scheduledDate,
+                                                const ConfigurationParameterValueMap &requestOverrideCfgValues) override;
+    QList<std::reference_wrapper<TaskToSubmit>> CreateTasks(const S4CBareSoilJobConfig &cfg, QList<TaskToSubmit> &outAllTasksList);
+    NewStepList CreateSteps(QList<TaskToSubmit> &allTasksList,
+                            const S4CBareSoilJobConfig &cfg);
+    QStringList GetS2CalibrationTaskArgs(const S4CBareSoilJobConfig &cfg, const QString &s2CalibPath);
+    QStringList GetS1CalibrationTaskArgs(const S4CBareSoilJobConfig &cfg, const QString &s2CalibPath, const QString &s1CalibPath);
+    QStringList GetS2ModelTaskArgs(const S4CBareSoilJobConfig &cfg, const QString &s2CalibPath, const QString &s2Results, const QString &outputModel, const QString &figImportancePath);
+    QStringList GetS1ModelTaskArgs(const S4CBareSoilJobConfig &cfg, const QString &s1CalibPath, const QString &s1Results, const QString &outputModel, const QString &figImportancePath);
+    QStringList GetMarkersTaskArgs(const S4CBareSoilJobConfig &cfg, const QString &s2Results, const QString &s1Results, const QString &outputS2Markers,
+                                   const QString &outputS1Markers, const QString &outputAllMarkers);
+    QStringList GetProductFormatterArgs(TaskToSubmit &productFormatterTask, const S4CBareSoilJobConfig &cfg, const QStringList &listFiles);
+};
+
