e799febf4b61b3cb651e51f55409058c92be8c35
Laurențiu Nicola <lnicola@dend.ro>
2024-06-18 16:19:07 +0300

    Don't delete build dir

--- docker/app/entry.sh
+++ docker/app/entry.sh
@@ -2,10 +2,7 @@
 set -e
 
 cd sen2agri/packaging
-rm -rf Sen2AgriPlatform \
-       Sen2AgriApp
-rm -rf Sen2AgriRPM/sen2agri-app-*.rpm \
-       Sen2AgriRPM/sen2agri-downloaders-demmaccs-*.rpm
+rm -f Sen2AgriRPM/sen2agri-app-*.rpm
 
 mkdir -p Sen2AgriRPM
 
