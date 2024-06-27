8118fba1b401c4e62e63a92163298a87f93f7e0c
Laurențiu Nicola <lnicola@dend.ro>
2024-06-27 16:38:24 +0300

    Clean up and upgrade build image to Rocky 9

--- docker/build/docker-run.sh
+++ docker/build/docker-run.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 docker build \
     -t sen2agri-build \
-    --build-arg user=$USER \
-    --build-arg group=$(id -ng) \
-    --build-arg uid=$(id -u) \
-    --build-arg gid=$(id -g) \
     .
 
 docker run -it --rm \
        -v $(realpath ../..):/sen2agri \
-       -u $(id -u):$(id -g) \
        sen2agri-build /bin/bash entry.sh
