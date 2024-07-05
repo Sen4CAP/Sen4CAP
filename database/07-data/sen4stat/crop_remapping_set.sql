30da9a86232df64fc80a901138d78f3f45f8faf4
Laurențiu Nicola <lnicola@dend.ro>
2024-07-05 14:56:57 +0300

    Rename default remapping set

--- database/07-data/sen4stat/crop_remapping_set.sql
+++ database/07-data/sen4stat/crop_remapping_set.sql
@@ -1,5 +1,5 @@
 COPY public.crop_remapping_set (crop_remapping_set_id, name) FROM stdin;
-1	Senegal pre-classification grouping
+1	Default crop remapping set
 \.
 
 SELECT pg_catalog.setval('public.crop_remapping_set_crop_remapping_set_id_seq', 1, true);
