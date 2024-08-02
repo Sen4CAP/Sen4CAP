f0cf45315f7a342936e9913a8bdd53096569f026
Cosmin Udroiu <cosmin.udroiu@c-s.ro>
2024-08-02 14:16:50 +0300

    Updated to add docker workaround networking scheme

--- scripts/export-product-launcher.py
+++ scripts/export-product-launcher.py
@@ -36,6 +36,9 @@ class Config(object):
         self.user = parser.get("Database", "UserName")
         self.password = parser.get("Database", "Password")
 
+        # work around Docker networking scheme
+        if self.host == "127.0.0.1" or self.host == "::1" or self.host == "localhost":
+            self.host = "172.17.0.1"
 
 def get_practice(name):
     if name == "NA":
